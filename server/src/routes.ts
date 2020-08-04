d8a835c8f677c3116770a25ac2d887320ff239cf
Brunno Sena <brunnow@hotmail.com>
2020-08-04 12:29:53 -0300

    Second Day

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import ClassesController from './controllers/Classes';
+import ConnectionsController from './controllers/Connections';
+
+const routes = express.Router();
+
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesController.index)
+routes.post('/classes', classesController.create)
+
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
+
+
+
+
+export default routes;
